a4bc219774021700b812e7efa265b0a630b6b154
Eduardo A.M <eduardo.maciel@edu.univali.br>
2023-07-27 01:08:08 -0300

    add useBlobity

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,7 +1,26 @@
+import { useEffect } from "react";
 import Button from "../Button";
 import SlideLogos from "../SlideLogos";
+import useBlobity from "blobity/lib/react/useBlobity";
+import { initialBlobityOptions } from "../../config/initialBlobityOptions";
 
 export default function Hero() {
+   const blobityInstance = useBlobity(initialBlobityOptions);
+
+   useEffect(() => {
+      if (blobityInstance.current) {
+         // @ts-ignore for debugging purposes or playing around
+         window.blobity = blobityInstance.current;
+      }
+   }, [blobityInstance]);
+
+   useEffect(() => {
+      window.scrollTo({
+         top: 0,
+         left: 0,
+      });
+   }, []);
+
    return (
       <div className='bg-black w-full mt-[-128px]'>
          <div className='h-screen w-full flex flex-col'>
